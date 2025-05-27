18c60322003ababa0a4a42b02bf705f26f08ca2c
zmiricz <zoltan.miricz@gmail.com>
2025-05-27 20:33:24 +0000

    XP multiplier and Vantos BH trigger

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -54,17 +54,17 @@ performanceDuration = 7200 -- in seconds
 medicalDuration = 7200 -- in seconds
 
 --Sets the experience multiplier while grouped
-groupExpMultiplier = 2.0
+groupExpMultiplier = 5.0
 
 --Sets a global experience multiplier
-globalExpMultiplier = 20.0
+globalExpMultiplier = 30.0
 
 --Sets the base number of control devices of each type that a player can have in their datapad at once
 --For creature pets, A Creature Handler will have the base number + their stored pets skill mod as limit
 baseStoredCreaturePets = 2
 baseStoredFactionPets = 3
 baseStoredDroids = 5
-baseStoredVehicles = 3
+baseStoredVehicles = 5
 baseStoredShips = 3
 
 -----------------------------------
